d42e1ff19d82f13fabf5628442bef482d604886d
Hilman Taris Muttaqin <hilmanmuttaqin345@gmail.com>
2022-02-03 18:23:23 +0700

    Add function for prepare channel in single instance

--- app/src/main/java/id/example/simplenotification/MainActivity.kt
+++ app/src/main/java/id/example/simplenotification/MainActivity.kt
@@ -15,6 +15,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        prepareNotificationChannel(NOTIFICATION_CHANNEL_ID, "Chat Notification", "Testing description for notification channel android")
+
         var incrementNotificationID = 0
 
         val buttonNotification = findViewById<Button>(R.id.btnNotification)
@@ -24,9 +26,6 @@ class MainActivity : AppCompatActivity() {
             // Apabila tidak di update maka notifikasi baru akan menimpa notifikasi lama
             val notificationData = NotificationData(
                 notificationId = NOTIFICATION_CHAT,
-                channelId = NOTIFICATION_CHANNEL_ID,
-                channelName = "Chat Notification",
-                channelDescription = "Testing description for notification channel android",
                 ctx = applicationContext,
                 title = "Trigger $incrementNotificationID",
                 body = "lorem ipsum dolor sit amet $incrementNotificationID"
@@ -37,28 +36,31 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    fun prepareNotificationChannel(channelId: String, channelName: String, channelDesc: String) {
+        val mNotificationManager: NotificationManager = applicationContext
+            .getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            //  Starting in Android 8.0 (API level 26), all notifications must be assigned to a channel
+            val channel = NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT)
+            channel.apply {
+                description = channelDesc
+            }
+            mNotificationManager.createNotificationChannel(channel)
+        }
+    }
+
     fun showNotification(notificationData: NotificationData) {
         val mNotificationManager: NotificationManager = notificationData.ctx
             .getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
 
-        val builder = NotificationCompat.Builder(notificationData.ctx, notificationData.channelId).apply {
+        val builder = NotificationCompat.Builder(notificationData.ctx, NOTIFICATION_CHANNEL_ID).apply {
             setContentTitle(notificationData.title)
             setContentText(notificationData.body)
             setSmallIcon(R.drawable.ic_baseline_notifications_24)
             priority = NotificationCompat.PRIORITY_DEFAULT
             setStyle(NotificationCompat.BigTextStyle())
-        }
-
-
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            //  Starting in Android 8.0 (API level 26), all notifications must be assigned to a channel
-            val channel = NotificationChannel(notificationData.channelId, notificationData.channelName, NotificationManager.IMPORTANCE_DEFAULT)
-            channel.apply {
-                description = notificationData.channelDescription
-            }
-            builder.setChannelId(notificationData.channelId)
-            mNotificationManager.createNotificationChannel(channel)
         }
 
         val notification = builder.build()
--- app/src/main/java/id/example/simplenotification/NotificationData.kt
+++ app/src/main/java/id/example/simplenotification/NotificationData.kt
@@ -6,12 +6,6 @@ data class NotificationData(
 
     val notificationId: Int = 101,
 
-    val channelId: String = "channel_01",
-
-    val channelName: String = "notification channel",
-
-    val channelDescription: String = "channel description",
-
     val ctx: Context,
 
     val title: String,
